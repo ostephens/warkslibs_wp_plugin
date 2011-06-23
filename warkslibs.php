94ee84a59eca53e3728ad10ca53b4a10ef7170c5
Owen Stephens <owen@ostephens.com>
2011-06-23 08:42:18 +0100

    Initial setup cont.

---
+++ warkslibs.php
@@ -0,0 +1,215 @@
+<?php
+/*
+Plugin Name: Warwickshire Libraries
+Plugin URI: https://github.com/ostephens/savelibs
+Description: WordPress plugin to add items from Warwickshire libraries to a blogpost
+Version: 0.1
+Author: Owen Stephens
+License: GPL2
+
+*/
+
+/*
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+/* To Do and Notes
+1. Really the page created by the shortcode should only be visible to logged in users with appropriate permissions - perhaps should be in Admin area?
+*/
+
+
+/////////// set up activation and deactivation stuff
+register_activation_hook(__FILE__,'warkslibs_install');
+
+function warkslibs_install() {
+	// do stuff when installed
+	global $wp_version;
+	if (version_compare($wp_version, "3", "<")) {
+		deactivate_plugins(basename(__FILE__)); // deactivate plugin
+		wp_die("This plugin requires WordPress Version 3 or higher.");
+		// ???relies on curl so check for it 
+		// if going to use simple web need to check for it - complicated?
+	} else {
+		// What here?
+	}
+}
+
+
+register_deactivation_hook(__FILE__,'warkslibs_uninstall');
+
+function warkslibs_uninstall() {
+	// do stuff
+}
+
+
+/////////// set up option storing stuff
+// create array of options
+// Want this to be repeatable values of barcode/pin so can support for multiple accounts
+// For now assume just one account
+// Note that this didn't work for Mia...
+
+$warkslibs_options_arr=array(
+	"warkslibs_user_barcode"=>'',
+	"warkslibs_user_pin"=>'',
+	);
+
+// store them
+update_option('warkslibs_plugin_options',$warkslibs_options_arr); 
+
+// get them
+$warkslibs_options_arr = get_option('warkslibs_plugin_options');
+
+// use them. 
+$warkslibs_user_barcode = $warkslibs_options_arr["warkslibs_user_barcode"];
+$warkslibs_user_pin = $warkslibs_options_arr["warkslibs_user_pin"];
+// end option array setup
+
+// required in WP 3 but not earlier?
+add_action('admin_menu', 'warkslibs_plugin_menu');
+
+/////////// set up stuff for admin options pages
+// add submenu item to existing WP menu
+function warkslibs_plugin_menu() {
+	add_options_page('Warwickshire Libraries Settings Page', 'Warwickshire Libraries Settings', 'manage_options', __FILE__, 'warkslibs_settings_page');
+}
+
+// call register settings function before admin pages rendered
+add_action('admin_init', 'warkslibs_register_settings');
+
+function warkslibs_register_settings() {
+	// register settings - array, not individual
+	register_setting('warkslibs-settings-group', 'warkslibs_settings_values');
+}
+
+// write out the plugin options form. Form field name must match option name.
+// add other options here as necessary e.g. new API URLs or updated defaults
+function warkslibs_settings_page() {
+  
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+
+	?>
+		<div>
+		<h2><?php _e('share what you see plugin options', 'warkslibs-plugin') ?></h2>
+		<form method="post" action="options.php">
+		<?php settings_fields('warkslibs-settings-group'); ?>
+
+  		<?php _e('Barcode (from your Warwickshire Library card)','warkslibs-plugin') ?> 
+  
+  		<?php warkslibs_user_barcode(); ?><br />
+
+  		<?php _e('PIN (usually your date of birth)','warkslibs-plugin') ?> 
+  
+  		<?php warkslibs_user_pin(); ?><br />
+
+  		<p class="submit"><input type="submit" class="button-primary" value=<?php _e('Save changes', 'warkslibs-plugin') ?> /></p>
+  		</form>
+  		</div>
+
+<?php
+}
+
+// get options from array and display as fields
+
+function warkslibs_user_barcode() {
+	// load options array
+	$warkslibs_options = get_option('warkslibs_settings_values');
+  
+	$warkslibs_user_barcode = $warkslibs_options['warkslibs_user_barcode'];
+  
+	// display form field
+	echo '<input type="text" name="warkslibs_settings_values[warkslibs_user_barcode]" 
+		value="'.esc_attr($warkslibs_user_barcode).'" />';
+}
+
+function warkslibs_user_pin() {
+	// load options array
+	$warkslibs_options = get_option('warkslibs_settings_values');
+  
+	$warkslibs_user_pin = $warkslibs_options['warkslibs_user_pin'];
+  
+	// display form field
+	echo '<input type="text" name="warkslibs_settings_values[warkslibs_user_pin]" 
+		value="'.esc_attr($warkslibs_user_pin).'" />';
+}
+
+
+/*
+ * set up shortcode Sample: [warkslibs]
+ */
+function warkslibsShortCode($atts, $content=null) {
+  
+	if(@is_file(ABSPATH.'/wp-content/plugins/warkslibs/warkslibs_functions.php')) {
+		include_once(ABSPATH.'/wp-content/plugins/warkslibs/warkslibs_functions.php'); 
+	}
+	// Want to have Search Options and Loan History and Current Loans
+	// Focus on Loan History/Current Loans first
+	//
+  	// $search_terms = stripslashes($_POST['search_term']); // the free-text search field
+  	// $search_title = stripslashes($_POST['search_title']); // the free-text search field
+	$loans = $_POST['loans'];
+	$blogem = $_POST['blogem'];
+
+
+	if ($loans == "history") {
+    	// process - deal with search, display results and import into db
+		echo '<p>Searching now...</p>';  
+		warkslibsGetLoanHistory();
+	} elseif ($loans == "current") {
+		echo '<p>Searching now ..</p>';
+		warkslibsGetCurrentLoans();
+	} elseif ($blogem == "yes"){
+		// This is single post creation - want to enable creation of multiple posts - e.g. 'select all' from loan history and post
+		// This implies an array rather than a string
+		foreach($_POST['libitem'] as $item)  {
+			/*
+			if ($item[choose] = 1) {
+				echo "\n";
+				print_r($item);
+				echo "\n".$item[title]."\n";
+				$libitem['title'] = $item[title];
+				$libitem['auth'] = $item[auth];
+				$libitem['image'] = $item[image];
+				$libitem['isbn'] = $item[isbn];
+				$libitem['id'] = $item[id];
+				$libitem['price'] = 0;
+			}
+	    	$warkslibs_new_post_IDs = createwarkslibsPost($libitem);
+			*/
+			if ($item[choose]){
+				$libitem['title'] = $item[title];
+				$libitem['auth'] = $item[auth];
+				$libitem['image'] = $item[image];
+				$libitem['isbn'] = $item[isbn];
+				$libitem['id'] = $item[id];
+				$libitem['price'] = 0;
+				$warkslibs_new_post_IDs = createwarkslibsPost($libitem);
+			}
+	    //	echo '<h3>Added '.$object_url.'</h3>';
+		//	echo '<a href="../wp-admin/post.php?post='.$warkslibs_new_post_ID.'&action=edit">Go Edit</a>';			
+		}
+  	} else {
+    // display search box and instructions
+    warkslibsPrintSearchForm();
+  }
+}
+
+// Add the shortcode
+add_shortcode('warkslibs', 'warkslibsShortCode');
+
+?>
---
+++ warkslibs_functions.php
@@ -0,0 +1,299 @@
+<?php
+/*
+ * Functions for the Warwickshire Libraries plugin
+ * 
+ * File information:
+ * Contains functions to query the APIs for the terms provided
+ * and other stuff I need to document here!
+ */
+
+/*
+ * Notes on import adaptations:
+ * 
+ * Notes for Warwickshire libraries:
+ * 
+ * 
+ * 
+ * Import written for: WordPress 3 for generic WordPress installs
+ *  
+ */
+
+/*
+ * Needs adapting for Warwickshire Libraries ###
+ * Was: Specialised search box for Tag Duel - asks for period (time) and place [Mia]
+ * Might be nice to add optional free-text search if it's likely to return lots of results
+ * TO DO: 
+ */
+
+function warkslibsPrintSearchForm() {
+	?>
+	<h3>Find a book so you can share it in a blog post...</h3><form method="post" action="">
+	<input type="radio" name="loans" value="history" />Select from Loans History<br />
+	<input type="radio" name="loans" value="current" />Select from Current Loans
+	<input type="submit" name="search" value="Go!" />
+	</form>
+	<?php
+}
+
+
+/*
+ * Inputs: $terms is the search terms given in the box, $mode is display or import
+ */
+function warkslibsGetCurrentLoans() {
+	require_once('simple_html_dom.php');
+	// get barcode and PIN - just guessing how this works here (from warkslibs.php) - need to find out how to do this properly!
+	$warkslibs_options = get_option('warkslibs_settings_values');
+	$warkslibs_user_barcode = $warkslibs_options['warkslibs_user_barcode'];
+	$warkslibs_user_pin = $warkslibs_options['warkslibs_user_pin'];
+	$warkslibs_url = "http://library.warwickshire.gov.uk/vs/Pa.csp?OpacLanguage=eng&Profile=Default";
+	$login = getLogin($warkslibs_url,$warkslibs_user_barcode,$warkslibs_user_pin);
+	$currentLoans = getCurrent($login);
+	
+	$html = str_get_html($currentLoans);
+
+	foreach($html->find('td.listitemOdd',0)->parent()->parent()->find('tr') as $list) {
+		$libItem['title']     = $list->find('td', 1)->plaintext;
+		$libItem['barcode']   = $list->find('td', 2)->plaintext;
+		$libItem['dateBorrowed'] = $list->find('td', 4)->plaintext;
+		$libItem['dateDue']	= $list->find('td', 5)->plaintext;
+		$libItem['timesRenewed']	= $list->find('td', 6)->plaintext;
+		$libItem['charge']		= $list->find('td', 7)->plaintext;
+		if ($libItem['title'] === "Title") {
+			continue;
+		}
+		$libItems[] = $libItem;
+	}
+
+	foreach($libItems as $libItem) {
+		$xml = searchAquaB(urlencode($libItem['title']));
+		$doc = new DOMDocument();
+		$doc->loadXML($xml);
+		$xpath = new DOMXPath($doc);
+		$records = $doc->getElementsByTagName("record");
+		foreach($records as $record) {
+			$title = aquaBTitle($record,$xpath);
+			$titlekey = trim(preg_replace("/[^a-zA-Z ]/","",$title));
+			$searchtitlekey = trim(preg_replace("/[^a-zA-Z ]/","",$libItem['title']));
+			if (strcmp($titlekey,$searchtitlekey) != 0) {
+				continue;
+			}
+			
+			$libItem['title']     = $title;
+			$libItem['isbn']     = aquaBISBN($record,$xpath);
+			$libItem['auth']     = aquaBAuth($record,$xpath);
+			$libItem['image']     = aquaBImage($record,$xpath);
+			// ID retrieval not work at the moment
+			// Why not?
+			$libItem['id']     = aquaBID($record,$xpath);
+			
+			$libItems1[] = $libItem;
+		}
+	}
+	// At this point we have an array of arrays listing all of the current loans
+	// Non-unique titles have been expanded into multiple records
+	// The user can (in theory) choose the ones they actually have (or just the ones they want to blog)
+	// Then press 'blog these' (or similar) and create multiple blog posts (in draft)
+	// So at this point want to output stuff to the screen with all information included in form
+	// Then grab this all back when form submitted?
+	// Alternative is to just submit the relevant URLs of the form
+	// http://librarycatalogue.warwickshire.gov.uk/abwarwick/fullrecordinnerframe.ashx?hreciid=|library/vubissmart-marc|494187&output=xml
+	// And do a further retrieve to get the information needed - but this increases network traffic for no gain?
+	// See http://www.sitepoint.com/forums/php-34/$_post-ing-checkbox-values-corresponding-form-values-182722.html for how to do this
+	
+	// So - try outputting items
+	echo '<form method="post" action=""><input name="blogem" value="yes" id="blogem" type="hidden">';
+	echo '<ul>';
+	$no_libItems = count($libItems1);
+	for($i=0; $i<$no_libItems; $i++){
+		echo '<li>';
+		echo '<h3>'.$libItems1[$i]['title'].'</h3>';
+		// display cover image
+		echo "<img src=".$libItems1[$i]['image']." alt='Coverimage of ".$libItems1[$i]['title']."'>\n"; // need to check if it exists, otherwise?
+		// print the item title
+		// Do any of these need encoding? URL?
+		echo '<input name="libitem['.$i.'][choose]" value="1" id="choose" type="checkbox">'; // is 'id' required?
+		echo '<input name="libitem['.$i.'][title]" value="'.$libItems1[$i]['title'].'" id="title" type="hidden">'; // is 'id' required?
+		echo '<input name="libitem['.$i.'][isbn]" value="'.$libItems1[$i]['isbn'].'" id="isbn" type="hidden">'; // is 'id' required?
+		echo '<input name="libitem['.$i.'][auth]" value="'.$libItems1[$i]['auth'].'" id="auth" type="hidden">'; // is 'id' required?
+		echo '<input name="libitem['.$i.'][image]" value="'.$libItems1[$i]['image'].'" id="image" type="hidden">'; // is 'id' required?
+		echo '<input name="libitem['.$i.'][id]" value="'.$libItems1[$i]['id'].'" id="id" type="hidden">'; // is 'id' required?
+		echo '</li>';
+    }
+	echo '<input type="submit" name="itemstoblog" value="Blog these..." /></form><br />';
+    echo '</ul>';
+	
+/* This is where the Amazon Enhancement could go
+// Some idea of the code might be:
+$no_libItems = count($libItems1);
+for($i=0; $i<$no_libItems; $i++){
+	// Do Amazon lookup and enhance item (via function)
+}
+*/
+
+}
+
+function getLogin($url,$barcode,$pin) {
+	require_once("simpletest/browser.php");
+	// Should add in some kind of test?
+	$browser = &new SimpleBrowser();
+	$browser->get($url);	
+	$browser->setField('CardId',$barcode);
+	$browser->setField('Pin',$pin);
+	$browser->clickSubmitByName('SubmitButton');
+	return $browser;
+}
+
+function getCurrent($browser) {
+	require_once("simpletest/browser.php");
+	$browser->click('My loans and renewals');
+	$browser->setFrameFocus(3);
+	$currentloans_page = $browser->getContent();
+	return $currentloans_page;
+}
+
+function searchAquaB($search) {
+	$opacxml_baseurl = 'http://librarycatalogue.warwickshire.gov.uk/ABwarwick/result.ashx?branch=LEA&output=xml&q=';
+	$xml = file_get_contents($opacxml_baseurl.$search);
+	return $xml;
+}
+
+function aquaBTitle($record,$xpath) {
+	$xpath_title = "./fields/title";
+	$nodeList_title = $xpath->evaluate($xpath_title,$record);
+	if ($nodeList_title->length > 0) {
+		$content_title = $nodeList_title->item(0)->nodeValue;
+		$title = trim($content_title);
+	} else {
+		$title = "UNKNOWN TITLE";
+	}
+	return $title;
+}
+
+function aquaBISBN($record,$xpath) {
+	$xpath_isbn = "./fields/isbn/text()";
+	$nodeList_isbn = $xpath->evaluate($xpath_isbn,$record);
+	if ($nodeList_isbn->length > 0) {
+		$content_isbn = $nodeList_isbn->item(0)->nodeValue;
+		$isbn = $content_isbn;
+	} else {
+		$isbn = "UNKNOWN ISBN";
+	}
+	return $isbn;
+}
+
+function aquaBAuth($record,$xpath) {
+	$xpath_auth = "./fields/author/text()";
+	$nodeList_auth = $xpath->evaluate($xpath_auth,$record);
+	if ($nodeList_auth->length > 0) {
+		$content_auth = $nodeList_auth->item(0)->nodeValue;
+		$auth = $content_auth;
+	} else {
+		$auth = "UNKNOWN AUTHORS";
+	}
+	return $auth;
+}
+
+function aquaBImage($record,$xpath) {
+	$xpath_image = "./coverimageurl/text()";
+	$nodeList_image = $xpath->evaluate($xpath_image,$record);
+	if ($nodeList_image->length > 0) {
+		$content_image = $nodeList_image->item(0)->nodeValue;
+		$image = $content_image;
+	} else {
+		$image = "NO IMAGE";
+	}
+	return $image;
+}
+
+function aquaBID($record,$xpath) {
+	$xpath_id = "@extID";
+	$nodeList_id = $xpath->evaluate($xpath_id,$record);
+	if ($nodeList_id->length > 0) {
+		$content_id = $nodeList_id->item(0)->nodeValue;
+		$id = $content_id;
+	} else {
+		$id = "NO ID";
+	}
+}
+
+function searchAmazon($search) {
+	// Use https://github.com/Exeu/Amazon-ECS-PHP-Library/ or http://www.codediesel.com/php/accessing-amazon-product-advertising-api-in-php/
+	// Neither seems to support Power searching - which is what we need for ISBN search
+	// The latter doesn't seem to support book searching at all; The former requires SOAP module installed, and doesn't support Power
+	// So left with having to extend one, or just take the auth stuff and do the rest by hand
+	// This is all basically for price, and sometimes image...
+	/*
+	defined('AWS_API_KEY') or define('AWS_API_KEY', 'API KEY');
+	defined('AWS_API_SECRET_KEY') or define('AWS_API_SECRET_KEY', 'SECRET KEY');
+
+	require '../lib/AmazonECS.class.php';
+
+	try
+	{
+	    // get a new object with your API Key and secret key. Lang is optional.
+	    // if you leave lang blank it will be US.
+	    $amazonEcs = new AmazonECS(AWS_API_KEY, AWS_API_SECRET_KEY, 'UK');
+
+	    // from now on you want to have pure arrays as response
+	    $amazonEcs->returnType(AmazonECS::RETURN_TYPE_ARRAY);
+
+	    $response = $amazonEcs->responseGroup('ItemAttributes')->category('Books')->search('isbn:PHP 5');;
+	    //var_dump($response);
+
+	}
+	catch(Exception $e)
+	{
+	  echo $e->getMessage();
+	}
+	*/
+}
+
+function createWarksLibsPost($libItem) {
+	$content = "";
+	$image = $libItem['image'];
+	$title = $libItem['title'];
+	$price = $libItem['price'];
+	$link = $libItem['id']; //Need to add other stuff in here to form valid link to OPAC
+	$isbn = $libItem['isbn'];
+	// etc. etc.
+	
+	if (!empty($image)) {
+	$content .= "<img src=\"".$image."\" alt=\"".$title."\" />";
+	}
+	if (!empty($title)) {
+	$content .= "<strong>Title:</strong> ".$title."<br />";
+	}
+	if (!empty($description)) {
+	$content .= "<strong>Description:</strong> ".$description."<br />";
+	}
+	$new_post = array(
+	'post_title' => $title,
+	        'post_content' => convert_chars($content)
+	        //Default field values will do for the rest - so we don't need to worry about these - see http://codex.wordpress.org/Function_Reference/wp_insert_post
+	);
+
+	$post_id = wp_insert_post($new_post);
+
+	if (is_object($post_id)) {
+	//error - what to do?
+	return false;
+	}
+	elseif ($post_id == 0) {
+	//error - what to do?
+	return false;
+	}
+	else {
+	add_post_meta($post_id, 'object_title', $title); // Need to check it exists first - add this in
+	// add_post_meta($post_id, 'object_maker', $maker);
+	// add_post_meta($post_id, 'object_date', $date); 
+	add_post_meta($post_id, 'object_isbn', $isbn); // Need to check it exists first - add this in
+	//other custom fields here if required
+	add_post_meta($post_id, 'object_price', $price); // Need to check it exists first - add this in
+	
+	// What about loan/due date?
+	}
+	return $post_id;
+	
+}
+
+?>
